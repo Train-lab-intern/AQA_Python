e5e248c30c250d36115b5bccfb50553969731dc4
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2023-09-20 14:47:06 +0300

    fixed_pylint

--- api/test_registration/test_registration.py
+++ api/test_registration/test_registration.py
@@ -1,15 +1,14 @@
+import json
 import pytest
 import requests
 import allure
-import json
 
 
-domain = 'https://test.app.it-roast.com'
-register_user = '/api/v1/users/register'
-complete_registration = '/api/v1/users/complete-registration'
+DOMAIN = 'https://test.app.it-roast.com'
 
 
 def post_request_create_user(email, username, password):
+    register_user = '/api/v1/users/register'
     headers = {
         'Content-Type': 'application/json'
     }
@@ -18,16 +17,17 @@ def post_request_create_user(email, username, password):
         "email": f"{email}",
         "password": f"{password}"
     })
-    request = requests.request('POST', domain + register_user, headers=headers,
+    request = requests.request('POST', DOMAIN + register_user, headers=headers,
                                data=data)
     return request
 
 
 def get_request_confirm_registration(email):
+    complete_registration = '/api/v1/users/complete-registration'
     params = {
         'userEmail': f'{email}'
     }
-    request = requests.request('GET', domain + complete_registration, params=params)
+    request = requests.request('GET', DOMAIN + complete_registration, params=params)
     return request
 
 
@@ -66,8 +66,9 @@ def test_check_invalid_emails(connect_db, email, check_existence_and_delete_emai
 
 @allure.feature('Registration')
 @allure.story('Check username')
-@pytest.mark.parametrize('username', ['VASIA', 'vasia', '1vasia', 'Vasia,./<>?:;|{}[]~`!@#$%^&*()_+=-',
-                                      '', '   '])
+@pytest.mark.parametrize('username', [
+    'VASIA', 'vasia', '1vasia', 'Vasia,./<>?:;|{}[]~`!@#$%^&*()_+=-', '', '   '
+])
 @pytest.mark.parametrize('email', ['test.trainlab@gmail.com'])
 def test_username(connect_db, username, check_existence_and_delete_email, email):
     with allure.step('Send request to create user'):
@@ -78,11 +79,13 @@ def test_username(connect_db, username, check_existence_and_delete_email, email)
 
 @allure.feature('Registration')
 @allure.story('Send a request with an valid password')
-@pytest.mark.parametrize('password', ['Qaz1234567890123456789012345678901234567890123456789xexexcefcrgv'
-                                      'thc1234567890123556790qertyooasdfghkozxvbnmqeryoadfhjkzxcvbnmasdfgh'
-                                      'kqetyuiizsdfghjjkjzxcderfvtgbyhnqazwsx1234567890qwertyuioasdgghjklz'
-                                      'xcvbnmmececfvrvtgbhbybcedxwsxwdcefvrgtbrcexwxexrgvtvhbrcfe',
-                                      'Qaz12345', '12345azQ', '123Q45we'])
+@pytest.mark.parametrize('password', [
+    'Qaz1234567890123456789012345678901234567890123456789xexexcefcrgv'
+    'thc1234567890123556790qertyooasdfghkozxvbnmqeryoadfhjkzxcvbnmasdfgh'
+    'kqetyuiizsdfghjjkjzxcderfvtgbyhnqazwsx1234567890qwertyuioasdgghjklz'
+    'xcvbnmmececfvrvtgbhbybcedxwsxwdcefvrgtbrcexwxexrgvtvhbrcfe',
+    'Qaz12345', '12345azQ', '123Q45we'
+])
 @pytest.mark.parametrize('email', ['test.trainlab@gmail.com'])
 def test_valid_password(connect_db, password, email, check_existence_and_delete_email):
     with allure.step('Send request to create user'):
@@ -93,15 +96,17 @@ def test_valid_password(connect_db, password, email, check_existence_and_delete_
 
 @allure.feature('Registration')
 @allure.story('Send a request with an invalid password')
-@pytest.mark.parametrize('password', ['', '        ', '1 ', '   1234567    Q a ', '12345678',
-                                      'qazwsxedc', 'QWERASDFZ', 'Aqazwsxedc', 'Фйц123456',
-                                      'Qaz1234', 'Qaz12345678901234567890123456789012345678'
-                                                 '90123456789xexexcefcrgvthc12345678901235567'
-                                                 '90qertyooasdfghkozxvbnmqeryoadfhjkzxcvbnmasdfghk'
-                                                 'qetyuiizsdfghjjkjzxcderfvtgbyhnqazwsx1234567890qw'
-                                                 'ertyuioasdgghjklzxcvbnmmececfvrvtgbhbybcedxwsxwdce'
-                                                 'fvrgtbrcexwxexrgvtvhbrcfed', 'QAZ12345', 'Qaz12345`',
-                                      'Qaz12345,<.>/?;:~!@#$%^&*()_-+=|'])
+@pytest.mark.parametrize('password', [
+    '', '        ', '1 ', '   1234567    Q a ', '12345678',
+    'qazwsxedc', 'QWERASDFZ', 'Aqazwsxedc', 'Фйц123456',
+    'Qaz1234', 'Qaz12345678901234567890123456789012345678'
+    '90123456789xexexcefcrgvthc12345678901235567'
+    '90qertyooasdfghkozxvbnmqeryoadfhjkzxcvbnmasdfghk'
+    'qetyuiizsdfghjjkjzxcderfvtgbyhnqazwsx1234567890qw'
+    'ertyuioasdgghjklzxcvbnmmececfvrvtgbhbybcedxwsxwdce'
+    'fvrgtbrcexwxexrgvtvhbrcfed', 'QAZ12345', 'Qaz12345`',
+    'Qaz12345,<.>/?;:~!@#$%^&*()_-+=|'
+])
 @pytest.mark.parametrize('email', ['test.trainlab@gmail.com'])
 def test_invalid_password(connect_db, password, email, check_existence_and_delete_email):
     with allure.step('Send request to create user'):
@@ -151,9 +156,9 @@ def test_registration_with_empty_username_and_email(connect_db):
 @pytest.mark.parametrize('email', ['test.trainlab@gmail.com'])
 def test_registration_with_existing_user(connect_db, check_existence_and_delete_email, email):
     with allure.step('Send request to create user'):
-        post_request_create_user(f'{email}', 'Vasia', f'Qaz12345')
+        post_request_create_user(f'{email}', 'Vasia', 'Qaz12345')
     with allure.step('Send a request with existing data'):
-        request = post_request_create_user(f'{email}', 'Vasia', f'Qaz12345')
+        request = post_request_create_user(f'{email}', 'Vasia', 'Qaz12345')
     with allure.step('Check status code'):
         assert request.status_code == 400
 
@@ -163,7 +168,7 @@ def test_registration_with_existing_user(connect_db, check_existence_and_delete_
 @pytest.mark.parametrize('email', ['test.trainlab@gmail.com'])
 def test_confirm_registration(connect_db, check_existence_and_delete_email, email):
     with allure.step('Send request to create user'):
-        post_request_create_user(f'{email}', 'Vasia', f'Qaz12345')
+        post_request_create_user(f'{email}', 'Vasia', 'Qaz12345')
     with allure.step('Send request confirm registration'):
         request = get_request_confirm_registration(f'{email}')
     with allure.step('Check status code'):
