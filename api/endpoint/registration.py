3be2add57e7f8569a80d3dfa0a7fbe8d709070d6
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2023-10-04 00:24:28 +0300

    create_endpoint-object

--- api/endpoint/registration.py
+++ api/endpoint/registration.py
@@ -7,14 +7,14 @@ class Registration(BaseMethod):
     def create_new_user(self, email, username, password):
         with allure.step('Send request to create user'):
             response = self.post_request_create_user(
-                f'{email}', username, password
+                email, username, password
             )
             return response
 
     def confirm_registration(self, email):
         with allure.step('Send request confirm registration'):
-            return self.get_request_confirm_registration(f'{email}')
+            return self.get_request_confirm_registration(email)
 
     def authentication(self, email, password):
         with allure.step('Send a login request'):
-            return self.post_request_authentication(f'{email}', password)
+            return self.post_request_authentication(email, password)
