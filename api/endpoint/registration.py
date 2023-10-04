1c4defdc3809a3fdd88717df068dce1b2be109cb
SiarheiZhachek <SiarheiZhachek@yandex.ru>
2023-10-04 16:35:37 +0300

    add_method_in_registration

--- api/endpoint/registration.py
+++ api/endpoint/registration.py
@@ -4,17 +4,69 @@ from api.endpoint.base_method import BaseMethod
 
 class Registration(BaseMethod):
 
-    def create_new_user(self, email, username, password):
+    def __init__(self, email, username, password):
+        super().__init__()
+        self.confirm_response = None
+        self.response = None
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def create_new_user(self):
         with allure.step('Send request to create user'):
-            response = self.post_request_create_user(
-                email, username, password
+            self.response = self.post_request_create_user(
+                self.email, self.username, self.password
             )
-            return response
+            return self.response
+
+    def returned_200(self):
+        with allure.step('Check status code 200'):
+            return self.response.status_code == 200
+
+    def returned_text_registration_initiated(self):
+        with allure.step('Check response text'):
+            return self.response.text == 'Registration initiated.' \
+                                        ' Please check your email for further instructions.'
+
+    def returned_text_registration_completed(self):
+        with allure.step('Check response text'):
+            return self.response.text == 'Registration completed successfully!'
+
+    def returned_message_invalid_email_address(self):
+        with allure.step('Check response message'):
+            response_data = self.response.json()
+            message = response_data['message']
+            return message == 'Invalid email address'
+
+    def returned_message_email_must_be_between_8_and_256_characters(self):
+        with allure.step('Check response message'):
+            response_data = self.response.json()
+            message = response_data['message']
+            return message == 'User email must be between 8 and 256 characters'
+
+    def returned_message_user_exists(self):
+        with allure.step('Check response message'):
+            response_data = self.response.json()
+            message = response_data['message']
+            return message == 'User with this username is already exists.'
+
+    def returned_message_user_mot_found(self, email):
+        with allure.step('Check response message'):
+            response_data = self.response.json()
+            message = response_data['message']
+            return message == f'User not found with email: {email}'
+
+    def returned_status_bad_request(self):
+        with allure.step('Check response status'):
+            response_data = self.response.json()
+            status = response_data['status']
+            return status == 'BAD_REQUEST'
+
+    def returned_400(self):
+        with allure.step('Check status code 400'):
+            return self.response.status_code == 400
 
     def confirm_registration(self, email):
         with allure.step('Send request confirm registration'):
-            return self.get_request_confirm_registration(email)
-
-    def authentication(self, email, password):
-        with allure.step('Send a login request'):
-            return self.post_request_authentication(email, password)
+            self.response = self.get_request_confirm_registration(email)
+            return self.response
--- api/tests/test_registration.py
+++ api/tests/test_registration.py
@@ -8,25 +8,21 @@ from api import test_data
 @allure.story('Send a request with a valid email')
 @pytest.mark.parametrize('email', test_data.VALID_EMAILS)
 def test_check_valid_emails(connect_db, email, check_existence_and_delete_email):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(email, test_data.USERNAME, test_data.PASSWORD)
-    assert response.status_code == 200
-    assert response.text == 'Registration initiated.' \
-                            ' Please check your email for further instructions.'
+    register_endpoint = Registration(email, test_data.USERNAME, test_data.PASSWORD)
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_200()
+    assert register_endpoint.returned_text_registration_initiated()
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with an invalid email address')
 @pytest.mark.parametrize('email', test_data.INVALID_EMAILS)
 def test_check_invalid_emails(connect_db, email, check_existence_and_delete_email):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(email, test_data.USERNAME, test_data.PASSWORD)
-    response_data = response.json()
-    message = response_data['message']
-    status = response_data['status']
-    assert response.status_code == 400
-    assert message == 'Invalid email address'
-    assert status == 'BAD_REQUEST'
+    register_endpoint = Registration(email, test_data.USERNAME, test_data.PASSWORD)
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_message_invalid_email_address()
+    assert register_endpoint.returned_status_bad_request()
 
 
 @allure.feature('Registration')
@@ -34,11 +30,10 @@ def test_check_invalid_emails(connect_db, email, check_existence_and_delete_emai
 @pytest.mark.parametrize('username', test_data.USERNAMES)
 @pytest.mark.parametrize('email', [test_data.EMAIL])
 def test_username(connect_db, username, check_existence_and_delete_email, email):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(email, username, test_data.PASSWORD)
-    assert response.status_code == 200
-    assert response.text == 'Registration initiated.' \
-                            ' Please check your email for further instructions.'
+    register_endpoint = Registration(email, username, test_data.PASSWORD)
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_200()
+    assert register_endpoint.returned_text_registration_initiated()
 
 
 @allure.feature('Registration')
@@ -46,11 +41,10 @@ def test_username(connect_db, username, check_existence_and_delete_email, email)
 @pytest.mark.parametrize('password', test_data.VALID_PASSWORD)
 @pytest.mark.parametrize('email', [test_data.EMAIL])
 def test_valid_password(connect_db, password, email, check_existence_and_delete_email):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(email, test_data.USERNAME, password)
-    assert response.status_code == 200
-    assert response.text == 'Registration initiated.' \
-                            ' Please check your email for further instructions.'
+    register_endpoint = Registration(email, test_data.USERNAME, password)
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_200()
+    assert register_endpoint.returned_text_registration_initiated()
 
 
 @allure.feature('Registration')
@@ -58,85 +52,67 @@ def test_valid_password(connect_db, password, email, check_existence_and_delete_
 @pytest.mark.parametrize('password', test_data.INVALID_PASSWORD)
 @pytest.mark.parametrize('email', [test_data.EMAIL])
 def test_invalid_password(connect_db, password, email, check_existence_and_delete_email):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(email, test_data.USERNAME, password)
-    body = response.json()
-    status = body['status']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
+    register_endpoint = Registration(email, test_data.USERNAME, password)
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with empty fields')
 def test_registration_with_empty_fields(connect_db):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(
+    register_endpoint = Registration(
         test_data.EMPTY_FIELD, test_data.EMPTY_FIELD, test_data.EMPTY_FIELD
     )
-    body = response.json()
-    status = body['status']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with empty username and password')
 def test_registration_with_empty_username_and_password(connect_db):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(
+    register_endpoint = Registration(
         test_data.EMAIL, test_data.EMPTY_FIELD, test_data.EMPTY_FIELD
     )
-    body = response.json()
-    status = body['status']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with empty email and password')
 def test_registration_with_empty_email_and_password(connect_db):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(
+    register_endpoint = Registration(
         test_data.EMPTY_FIELD, test_data.USERNAME, test_data.EMPTY_FIELD
     )
-    body = response.json()
-    status = body['status']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with empty username and email')
 def test_registration_with_empty_username_and_email(connect_db):
-    register_endpoint = Registration()
-    response = register_endpoint.create_new_user(
+    register_endpoint = Registration(
         test_data.EMPTY_FIELD, test_data.EMPTY_FIELD, test_data.PASSWORD
     )
-    body = response.json()
-    status = body['status']
-    message = body['message']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
-    assert message == 'User email must be between 8 and 256 characters'
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
+    assert register_endpoint.returned_message_email_must_be_between_8_and_256_characters
 
 
 @allure.feature('Registration')
 @allure.story('Send a request with data existing user')
 @pytest.mark.parametrize('email', [test_data.EMAIL])
 def test_registration_with_existing_user(connect_db, check_existence_and_delete_email, email):
-    register_endpoint = Registration()
-    register_endpoint.create_new_user(
-        email, test_data.USERNAME, test_data.PASSWORD
-    )
-    response = register_endpoint.post_request_create_user(
-        email, test_data.USERNAME, test_data.PASSWORD
-    )
-    body = response.json()
-    status = body['status']
-    message = body['message']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
-    assert message == 'User with this username is already exists.'
+    register_endpoint = Registration(email, test_data.USERNAME, test_data.PASSWORD)
+    register_endpoint.create_new_user()
+    register_endpoint.create_new_user()
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
+    assert register_endpoint.returned_message_user_exists()
 
 
 @allure.feature('Registration')
@@ -145,13 +121,13 @@ def test_registration_with_existing_user(connect_db, check_existence_and_delete_
 def test_confirm_registration(
         connect_db, check_existence_and_delete_email, email, delete_session
 ):
-    register_endpoint = Registration()
-    register_endpoint.create_new_user(
+    register_endpoint = Registration(
         email, test_data.USERNAME, test_data.PASSWORD
     )
-    response = register_endpoint.confirm_registration(email)
-    assert response.status_code == 200
-    assert response.text == 'Registration completed successfully!'
+    register_endpoint.create_new_user()
+    register_endpoint.confirm_registration(email)
+    assert register_endpoint.returned_200()
+    assert register_endpoint.returned_text_registration_completed()
 
 
 @allure.feature('Registration')
@@ -160,14 +136,9 @@ def test_confirm_registration(
 def test_confirm_registration_with_invalid_email(
         connect_db, check_existence_and_delete_email, email, delete_session
 ):
-    register_endpoint = Registration()
-    register_endpoint.create_new_user(
-        email, test_data.USERNAME, test_data.PASSWORD
-    )
-    response = register_endpoint.confirm_registration(test_data.NON_EXISTENT_EMAIL)
-    body = response.json()
-    status = body['status']
-    message = body['message']
-    assert response.status_code == 400
-    assert status == 'BAD_REQUEST'
-    assert message == f'User not found with email: {test_data.NON_EXISTENT_EMAIL}'
+    register_endpoint = Registration(email, test_data.USERNAME, test_data.PASSWORD)
+    register_endpoint.create_new_user()
+    register_endpoint.confirm_registration(test_data.NON_EXISTENT_EMAIL)
+    assert register_endpoint.returned_400()
+    assert register_endpoint.returned_status_bad_request()
+    assert register_endpoint.returned_message_user_mot_found(test_data.NON_EXISTENT_EMAIL)
