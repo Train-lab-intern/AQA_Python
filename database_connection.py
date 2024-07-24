349d8bb30c724cb7b7c2d0c1a1d33806584bb63c
Siarhei Zhachek <106345484+SiarheiZhachek@users.noreply.github.com>
2024-07-24 03:32:10 +0300

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -1,10 +1,1 @@
-username = 'trainlab'
-password = 'AVNS_yXtS-TsJq6YJmCAe19y'
-host = 'tldb-postgresql-fra1-81116-do-user-14346310-0.b.db.ondigitalocean.com'
-port = 25060
-database = 'trainlab'
 
-server_ip = '165.22.78.188'
-ssh_port = 22
-ssh_username = 'szhachek'
-ssh_private_key = '/Users/sergey/.ssh/id_ed25519'
